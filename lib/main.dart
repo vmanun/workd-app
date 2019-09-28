7947cb63dd8013293d8dbec3d45f46011a2607b5
vmanun <vmanuelns@hotmail.com>
2019-09-28 18:16:17 -0400

    Deleted logo widget; relocated to appTheme.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,6 @@ import 'package:workd/hoursCounter.dart';
 void main() => runApp(Workd());
 
 class Workd extends StatelessWidget {
-  final Widget logo = Container(
-    width: 105.0,
-    height: 50.0, 
-    child: Center(child: Image.asset('assets/images/logo.png'))
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +14,7 @@ class Workd extends StatelessWidget {
           elevation: 0.0,
           backgroundColor: AppTheme.bgColor,
           title: Center(
-            child: logo,
+            child: AppTheme.logo,
           ),
         ),
         body: HoursCounter(),
