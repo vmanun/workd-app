7ddf01386e77d9f7f358635d3e411f5bab48bbbb
vmanun <vmanuelns@hotmail.com>
2019-10-04 18:26:03 -0400

    moved writeToJson and readToJson function to its own file added validation to the constructor for overwriting current session data added show error widget, shown when working for 1 hour or less

--- lib/resultsView.dart
+++ lib/resultsView.dart
@@ -1,36 +1,62 @@
-import 'dart:io';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:workd/appTheme.dart';
+import 'package:workd/hoursData.dart';
 
 class ResultsView extends StatelessWidget {
-
   final int totalHours;
 
-  ResultsView(this.totalHours) { writeToJson(); readFromJson(); } 
-
-  void writeToJson() async {
-    final directory = await getApplicationDocumentsDirectory();
-    var file = File('${directory.path}/data.json');
-    var newData = '{"date": "${DateTime.now().toString()}", "totalHours": "$totalHours"}';
-    List<dynamic> currentData = json.decode(file.readAsStringSync());
-    currentData.add(json.decode(newData));
-    file.writeAsString(json.encode(currentData), mode: FileMode.write);
+  ResultsView(this.totalHours, {bool overwriteSession = false}) {
+    if(totalHours != -1) {
+      if(!overwriteSession) writeToJson(totalHours);
+      else overwriteTodaySession(totalHours);
+    }
+    readFromJson();
   }
 
-  void readFromJson() async {
-    final directory = await getApplicationDocumentsDirectory();
-    var file = File('${directory.path}/data.json'); 
-    if(await file.exists()) {
-      var data = file.readAsStringSync();
-      print(data);
-    }   
+  Widget _showResults() {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Today, you\'ve worked for:',
+          style: AppTheme.headingOne,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$totalHours',
+              style: AppTheme.counterText,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'hours',
+              style: AppTheme.headingOne,
+              textAlign: TextAlign.center,
+            )
+          ],
+        )
+      ],
+    ));
+  }
+
+  Widget _showError() {
+    return Center(
+      child: Text(
+        'You\'ve worked for less than 1 hour, your hours won\'t be registered.',
+        style: AppTheme.headingOne,
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
           elevation: 0.0,
@@ -40,38 +66,8 @@ class ResultsView extends StatelessWidget {
             'Today\'s Results',
             textAlign: TextAlign.start,
             style: AppTheme.headingOne,
-          )
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Today, you\'ve worked for:',
-              style: AppTheme.headingOne,
-            ),
-             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  '$totalHours',
-                  style: AppTheme.counterText,
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'hours',
-                  style: AppTheme.headingOne,
-                  textAlign: TextAlign.center,
-                )
-              ],
-            )
-          ],
-        )
-      ),
+          )),
+      body: totalHours == -1 ? _showError() : _showResults(),
     );
   }
-} 
+}
