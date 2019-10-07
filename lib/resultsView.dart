734845bf663fef0140ef783ea12a2bfb0f66b744
vmanun <vmanuelns@hotmail.com>
2019-10-07 00:55:14 -0400

    Removed the today's session validatoin (now done at the overwriteTodaySession function) this view now handles the week's session results as well

--- lib/resultsView.dart
+++ lib/resultsView.dart
@@ -2,17 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:workd/appTheme.dart';
 import 'package:workd/hoursData.dart';
 
+/// Display both results for the session of the day nd the weeks' sessions
 class ResultsView extends StatelessWidget {
   final int totalHours;
+  final bool showWeekHours;
+  final List<dynamic> sessions;
 
-  ResultsView(this.totalHours, {bool overwriteSession = false}) {
-    if(totalHours != -1) {
-      if(!overwriteSession) writeToJson(totalHours);
-      else overwriteTodaySession(totalHours);
+  ResultsView(this.totalHours, {this.showWeekHours = false, this.sessions}) {
+    // -1 represents the totalHours being 0 or 1 hours. It has to be more thn one
+    // showWeekHours is the handler that turns this widget from today's sesion
+    // to the whole week's sessions
+    if(totalHours != -1 && !showWeekHours) {
+      writeToJson(totalHours);
     }
     readFromJson();
   }
+  /// Function that returns a Column widget with all the given week's sessions, or an empty Text if
+  /// showWeekHours is false
+  Widget _showSessions() {
+    List<Widget> sessionList = [];
 
+    if(sessions != null){
+      for(var session in sessions) {
+        var sessionDate = DateTime.parse(session['date']);
+        sessionList.add(
+        Text(
+            '${sessionDate.day}/${sessionDate.month}/${sessionDate.year}: ${session['totalHours']} hours',
+            style: AppTheme.dialogLight,
+          )
+        );
+      }
+    }
+
+    return showWeekHours ? Column(
+      children: <Widget>[
+        Divider(height: 20.0),
+        Text('Sessions',
+          style: AppTheme.dialog,
+        ),
+        Column(
+          children: sessionList,
+        )
+      ],
+    ) : Text('');
+  }
+  /// Returns the results: If showWeekHours is true, it displays the given week's
+  /// sessions (taken from the variable with that exact name); else, it will display
+  /// today's total worked hours. 
   Widget _showResults() {
     return Center(
         child: Column(
@@ -21,7 +57,7 @@ class ResultsView extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Text(
-          'Today, you\'ve worked for:',
+          '${showWeekHours ? 'This week' : 'Today'}, you\'ve worked for:',
           style: AppTheme.headingOne,
         ),
         Row(
@@ -40,11 +76,14 @@ class ResultsView extends StatelessWidget {
               textAlign: TextAlign.center,
             )
           ],
-        )
+        ),
+        _showSessions()
       ],
     ));
   }
 
+  /// Displays an error message, saying that the total hours must be at least
+  /// 1. Can be generalized if needed to display any post-hour-counting errors.
   Widget _showError() {
     return Center(
       child: Text(
@@ -63,7 +102,7 @@ class ResultsView extends StatelessWidget {
           backgroundColor: AppTheme.bgColor,
           iconTheme: IconThemeData(color: AppTheme.secondary),
           title: Text(
-            'Today\'s Results',
+            '${showWeekHours ? 'This Week' : 'Today'}\'s Results',
             textAlign: TextAlign.start,
             style: AppTheme.headingOne,
           )),
