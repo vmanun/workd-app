67c619f6499705982479a4e1ad5abe708f8062a6
vmanun <vmanuelns@hotmail.com>
2019-10-07 00:48:09 -0400

    Separated the Scaffold into itś own class Added an action button to the AppBar that leads to the weeks route

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:workd/appTheme.dart';
 import 'package:workd/hoursCounter.dart';
+import 'package:workd/weeksRoute.dart';
+import 'package:workd/appTheme.dart';
 
 void main() => runApp(Workd());
 
 class Workd extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
+
+/// Sets up the app's scaffold
+class HomeScreen extends StatelessWidget {
+  void _toWeekRoute(BuildContext context) => Navigator.push(context,
+      MaterialPageRoute(builder: (BuildContext context) => WeeksRoute()));
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          elevation: 0.0,
-          backgroundColor: AppTheme.bgColor,
-          title: Center(
-            child: AppTheme.logo,
-          ),
-          actions: <Widget>[
-            MaterialButton(
-              onPressed: (){},
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: AppTheme.bgColor,
+        title: Center(
+          child: AppTheme.logo,
+        ),
+        actions: <Widget>[
+          MaterialButton(
+              onPressed: () => _toWeekRoute(context),
               child: Icon(
                 Icons.calendar_today,
                 color: AppTheme.primary,
-              )
-            )
-          ],
-        ),
-        body: HoursCounter(),
-      )
+              ))
+        ],
+      ),
+      body: HoursCounter(),
     );
   }
 }
