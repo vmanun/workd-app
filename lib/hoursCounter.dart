2ccd5a2e7121790ba34a797d8bfa1659fee26def
vmanun <vmanuelns@hotmail.com>
2019-10-14 13:40:45 -0400

    changed the totalHours prop for both hours and minutes support

--- lib/hoursCounter.dart
+++ lib/hoursCounter.dart
@@ -2,62 +2,78 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:workd/appTheme.dart';
 import 'package:workd/resultsView.dart';
+import 'package:workd/manualHoursInput.dart';
 import 'package:workd/hoursData.dart';
 
 // TODO: The HoursCounter state needs some refactoring. Divide the counter from the first page
 /// The main view of the app. Displays and activates
 /// the counter when ready.
-class HoursCounter extends StatefulWidget {
+class HoursCounter extends StatefulWidget{
   HoursCounter();
 
   @override
   createState() => _HoursCounterState();
 }
 
-class _HoursCounterState extends State<HoursCounter> {
-  int _totalHours;
-  bool _isActive;
-  bool _isVisible;
-  bool _aSessionExistsToday;
-  Stream<int> _counter;
+class _HoursCounterState extends State<HoursCounter> with AutomaticKeepAliveClientMixin {
+  List<int>          _totalHours;
+  bool               _isActive = false;
+  bool               _isVisible;
+  bool               _aSessionExistsToday;
+  bool               _toManualInput = false;
+  Stream<int>        _counter;
   StreamSubscription _counterListener;
+  DateTime           _startTime;
 
   @override
   void initState() {
     super.initState();
-    _totalHours = 0;
+    _totalHours = [0, 0];
     _isActive = false;
+    updateKeepAlive();
     _isVisible = false;
+    _toManualInput = false;
   }
+
+  @override
+  bool get wantKeepAlive => _isActive;
 
   ///Starts counting the hours
   void _startCounter() => setState(() {
         _isVisible = true;
+        _startTime = DateTime.now();
         _counter = Stream.periodic(
             Duration(
-              hours: 1,
+              minutes: 1,
             ),
             (time) => time);
         _counterListener =
-            _counter.listen((t) => setState(() => _totalHours = t));
+            _counter.listen((t) => setState(() {
+              _totalHours[0] = DateTime.now().hour -_startTime.hour;
+              _totalHours[1] = DateTime.now().minute -_startTime.minute;
+            }));
         _fade(true);
       });
 
   ///stops and resets the counter state
   void _resetCounter() => setState(() {
         _isVisible = false;
+        _startTime = null; 
         _counterListener.cancel();
         _counterListener = null;
         _counter = null;
-        _totalHours = 0;
+        _totalHours[0] = 0;
+        _totalHours[1] = 0;
         _fade(false);
       });
 
   ///stops and opens the results screen
   void _finishCounter() => setState(() {
         _isVisible = false;
+        _totalHours[0] = DateTime.now().hour -_startTime.hour;
+        _totalHours[1] = DateTime.now().minute -_startTime.minute;
         _fade(false);
-        if (_totalHours > 1)
+        if (_totalHours[0] > 1)
           _toResultsRoute(false);
         else
           _toResultsRoute(true);
@@ -71,27 +87,37 @@ class _HoursCounterState extends State<HoursCounter> {
           context,
           MaterialPageRoute(
               builder: (BuildContext context) => ResultsView(
-                --_totalHours,
+                [--_totalHours[0], _totalHours[1]],
               )));
     } else {
       _counterListener.pause();
       await Navigator.push(
           context,
           MaterialPageRoute(
-              builder: (BuildContext context) => ResultsView(-1)));
+              builder: (BuildContext context) => ResultsView([-1, -1])));
     }
     setState(() {
       _counterListener.cancel();
       _counterListener = null;
       _counter = null;
-      _totalHours = 0;
+      _totalHours[0] = 0;
+      _totalHours[1] = 0;
     });
   }
 
+  void _toManualInputRoute(BuildContext context) => Navigator.push(
+    context,
+    MaterialPageRoute(
+        builder: (BuildContext context) => ManualHoursInput()));
+  
+
   ///A function that receives the visibility of the counter itself
-  void _fade(bool value) async => await Future.delayed(
+  void _fade([bool value = true]) async => await Future.delayed(
       const Duration(milliseconds: 200),
-      () => setState(() => _isActive = value));
+      () => setState(() {
+        _isActive = value;
+        updateKeepAlive();
+      }));
 
   ///The counter is ready to be displayed after pressing the BEGIN button
   Widget _counterReady() => AnimatedOpacity(
@@ -125,7 +151,34 @@ class _HoursCounterState extends State<HoursCounter> {
                       textAlign: TextAlign.center,
                       style: AppTheme.button,
                     ))),
-              ))
+              )),
+          MaterialButton(
+            onPressed: () async {
+              final sessionExists = await checkForSessionsToday();
+              if (!sessionExists) _toManualInputRoute(context);
+              setState(() {
+                _aSessionExistsToday = sessionExists;
+                _toManualInput = true;
+                });
+            },
+            minWidth: 0.0,
+            padding: EdgeInsets.symmetric(horizontal: 0.0),
+            splashColor: AppTheme.splashColor,
+            child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: AppTheme.secondary, width: 1.5),
+                  borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 0.0),
+                width: 155.0,
+                height: 55.0,
+                child: Center(
+                    child: Text(
+                  'ADD A SESSION MANUALLY',
+                  textAlign: TextAlign.center,
+                  style: AppTheme.button,
+                ))),
+            ),
         ],
       ));
 
@@ -149,12 +202,22 @@ class _HoursCounterState extends State<HoursCounter> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    '$_totalHours',
+                    '${_totalHours[0]}',
                     style: AppTheme.counterText,
                     textAlign: TextAlign.center,
                   ),
                   Text(
-                    'hours',
+                    'hours and',
+                    textAlign: TextAlign.center,
+                    style: AppTheme.headingOne,
+                  ),
+                  Text(
+                    '${_totalHours[1]}',
+                    style: AppTheme.counterText,
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    'mins',
                     textAlign: TextAlign.center,
                     style: AppTheme.headingOne,
                   ),
@@ -210,7 +273,8 @@ class _HoursCounterState extends State<HoursCounter> {
         FlatButton(
             onPressed: () => setState(() {
                 _aSessionExistsToday = false;
-                _startCounter();
+                if(_toManualInput) _toManualInputRoute(context);
+                else _startCounter();
               }),
             splashColor: AppTheme.splashColor,
             child: Text(
@@ -232,6 +296,7 @@ class _HoursCounterState extends State<HoursCounter> {
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return _aSessionExistsToday != null && _aSessionExistsToday ? 
            _showDialog() : 
            _isActive ? _counterActive() : _counterReady();
